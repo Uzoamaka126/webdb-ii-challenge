a9acdb6c6cea18a94a2cfa2e0733dcbfd27b1fef
Uzoamaka126 <vanessapeters126@gmail.com>
2019-11-05 18:30:41 +0100

    mvp completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const server = require('./api/server');
 
 const port = process.env.PORT || 5000;
+console.log(process.env.NODE_ENV);
+
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
